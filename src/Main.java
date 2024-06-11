b52747c3e980de02750544954b1099e4d01764c9
CassiaFernandaMessias <cassia.fernanda.dev@gmail.com>
2024-06-10 23:02:39 -0300

    BreakContinue

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+//+++++++++++++++++++++++BREAK+++++++++++++++++++++++++++++
+        //*for (int numero = 1; numero <= 5; numero++) {
+        //  if (numero == 3)
+        //   break;
+        //  System.out.println(numero);
+        //    }
+        // }
+        //}
+//++++++++++++++++++++CONTINUE++++++++++++++++++++++++++++++
+
+        //for (int numero = 1; numero <= 5; numero++) {
+        //  if (numero == 3)
+        //   continue;
+        //  System.out.println(numero);
+        //  }
+        //}
+    }
+}
